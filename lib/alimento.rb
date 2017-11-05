ff40fc01f69b880aa468f952292802b7dc68c440
Joram Real Gómez <alu0100226300@ull.edu.es>
2017-11-05 12:14:57 +0000

    Añadidas pruebas de la clase Lista

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -1,6 +1,7 @@
 require "alimento/version"
 require "alimento/alimentos"
 require "alimento/grupo"
+require "alimento/lista"
 
 module Alimento
   # Your code goes here...
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -162,4 +162,62 @@ describe Alimento do
 		expect(@nodo1.next).to equal(nil)
 	end
   end
+
+  context Lista do
+	before :each do			
+		@alimento1 = Alimentos.new("Huevo frito", 14.1, 0.0, 19.5)
+		@alimento2 = Alimentos.new("Leche vaca", 3.3, 4.8, 3.2)
+		@alimento3 = Alimentos.new("Yogurt", 3.8, 4.8, 3.8)
+		@alimento4 = Alimentos.new("Cerdo", 21.5, 0.0, 6.3)
+		@alimento5 = Alimentos.new("Ternera", 21.1, 0.0, 3.1)
+		@alimento6 = Alimentos.new("Pollo", 20.6, 0.0, 5.6)
+		@alimento7 = Alimentos.new("Bacalao", 17.7, 0.0, 0.4)
+		@alimento8 = Alimentos.new("Atún", 21.5, 0.0, 15.5)
+		@alimento9 = Alimentos.new("Salmón", 19.9, 0.0, 13.6)
+		@alimento10 = Alimentos.new("Aceite de oliva", 0.0, 0.2, 99.6)
+		@alimento11 = Alimentos.new("Chocolate", 5.3, 47.0, 30.0)
+		@alimento12 = Alimentos.new("Azúcar", 0.0, 99.8, 0.0)
+		@alimento13 = Alimentos.new("Arroz", 6.8, 77.7, 0.6)
+		@alimento14 = Alimentos.new("Lentejas", 23.5, 52.0, 1.4)
+		@alimento15 = Alimentos.new("Papas", 2.0, 15.4, 0.1)
+		@alimento16 = Alimentos.new("Tomate", 1.0, 3.5, 0.2)
+		@alimento17 = Alimentos.new("Cebolla", 1.3, 5.8, 0.3)
+		@alimento18 = Alimentos.new("Manzana", 0.3, 12.4, 0.4)
+		@alimento19 = Alimentos.new("Plátanos", 1.2, 21.4, 0.2)
+		@migrupo1 = Grupo.new(
+		"Huevos, lacteos y helados",
+		[ @alimento1, @alimento2, @alimento3 ])
+		@migrupo2 = Grupo.new(
+		"Carnes y derivados",
+		[ @alimento4, @alimento5, @alimento6 ])
+		@migrupo3 = Grupo.new(
+		"Pescados y mariscos",
+		[ @alimento7, @alimento8, @alimento9 ])
+		@migrupo4 = Grupo.new(
+		"Alimentos grasos",
+		[ @alimento10, @alimento11, @alimento12 ])
+		@lista1 = Lista.new(nil,nil)
+	end
+
+	it 'Debe existir una Lista con su cabeza' do
+	  	@lista1.insertar(@migrupo1)
+	      	expect(@lista1.cabeza.value).to eq(@migrupo1)
+	end
+
+	it 'Se puede insertar un elemento' do 
+        	@lista1.insertar(@migrupo1)
+	        expect(x = @lista1.cabeza.value).to equal(@migrupo1)
+  	end
+
+	it 'Se pueden insertar varios elementos' do
+      		@lista1.insertar_varios([@migrupo1,@migrupo2,@migrupo3,@migrupo4])
+		expect(x =@lista1.cabeza.value).to equal(@migrupo4)
+	end
+
+	it 'Se extrae el primer elemento de la lista' do
+        	@lista1.insertar_varios([@migrupo1,@migrupo2,@migrupo3,@migrupo4])
+	        @lista1.extraer_inicio()
+        	expect(x =@lista1.cabeza.value).to equal(@migrupo4)
+	end
+  end
 end
