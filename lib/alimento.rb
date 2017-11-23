da9995109bd18ac851ae87108a97b980d07dc604
Joram Real Gómez <alu0100226300@ull.edu.es>
2017-11-23 22:47:13 +0000

    Añadidas clases de la practica 5. Cambiado el calculo de aibc a funcional

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -2,6 +2,9 @@ require "alimento/version"
 require "alimento/alimentos"
 require "alimento/grupo"
 require "alimento/lista"
+require "alimento/comida"
+require "alimento/individuo"
+require "alimento/calculadora"
 
 module Alimento
   # Your code goes here...
